00c24c5cb40b9d0b7d11edc3b8c2df252d282273
limjunho <jewel7492@naver.com>
2020-08-26 15:05:47 +0900

    mysql select example

---
+++ mysql_select/README.txt
@@ -0,0 +1,5 @@
+mysql의 데이터를 가져와 출력하는 예제
+
+정리글
+https://limjunho.github.io/2020/08/26/MySql-Select-example.html
+
---
+++ mysql_select/mysql_select_ex.py
@@ -0,0 +1,18 @@
+import pymysql
+
+### Database connect ###
+conn = pymysql.connect(host='127.0.0.1', user='test', password='george7790',
+        db='test_db', charset='utf8')
+### get Cursor ###
+curs = conn.cursor(pymysql.cursors.DictCursor)
+### select CONST_COMMAND ###
+sql = """select * from test_table"""
+
+curs.execute(sql)
+
+# fetchone() - 한번 호출에 하나의 Row를 가져올 때 사용
+# fetchall() - 모든 데이터를 한꺼번에 가져올 때 사용
+# fetchmany(n) - n개 만큼의 데이터를 가져올 때 사용
+
+data = curs.fetchone()
+print(data)
